80bcab0e692beff8f003088ab907e4fd62f53fce
Artem <154594695+Nighty3098@users.noreply.github.com>
2025-09-07 00:27:07 -0500

    Added work exp. block

--- src/components/workexp.tsx
+++ src/components/workexp.tsx
@@ -1,6 +1,12 @@
 function WorkExp() {
   return (
-    <div className="WorkExp">
+    <div className="content-block about-block work-block" style={{ height: "100vh" }}>
+      <div style={{ display: "flex", flexDirection: "column", alignContent: "center", alignItems: "center", justifyContent: "center", textAlign: "left", gap: "var(--spacing-l)", padding: "0px", margin: "0px" }}>
+          <h2>My work experience</h2>
+          <h3>Freelance ( Jan 2024 - PT )</h3>
+          <p>Created over 10 serverless solutions using Vercel technologies and integrated them into modern web applications</p>
+          <p>Developed web applications and landing pages with a focus on user experience and responsive design</p>
+      </div>
     </div>
   );
 }
