438543ec4a69dbc65764bff8279d351530f936cc
Artem <154594695+Nighty3098@users.noreply.github.com>
2025-11-07 21:42:50 +0700

    Update welcome.tsx

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,126 +1,125 @@
 import { motion } from "framer-motion";
 
 const Navigation = () => {
-    return (
+  return (
+    <motion.div
+      className="navigation"
+      initial="hidden"
+      animate="visible"
+      variants={{
+        hidden: { opacity: 0, y: -20 },
+        visible: { opacity: 1, y: 0, transition: { staggerChildren: 0.2 } },
+      }}
+    >
+      {["PROJECTS", "CONTACTS", "PENTESTING"].map((text, index) => {
+        const getHref = (index: number) => {
+          if (index === 0) return "#projects";
+          if (index === 1) return "#my-contacts";
+          return "/pentesting";
+        };
+
+        return (
+          <motion.a
+            key={text}
+            href={getHref(index)}
+            variants={{
+              hidden: { opacity: 0, y: 10 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            whileHover={{ letterSpacing: "10px", color: "var(--accent)" }}
+            style={{ margin: "0 10px" }}
+          >
+            {text}
+          </motion.a>
+        );
+      })}
+    </motion.div>
+  );
+};
+
+function WelcomePage() {
+  return (
+    <motion.div
+      className="welcome-block content"
+      initial="hidden"
+      animate="visible"
+      style={{ backgroundColor: "var(--bg-2)" }}
+      variants={{
+        hidden: { opacity: 0, y: 20 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      transition={{ duration: 0.5 }}
+    >
+      <Navigation />
       <motion.div
-        className="navigation"
+        className="welcome-tiling"
         initial="hidden"
         animate="visible"
         variants={{
-          hidden: { opacity: 0, y: -20 },
-          visible: { opacity: 1, y: 0, transition: { staggerChildren: 0.2 } },
+          hidden: { opacity: 0, x: -20 },
+          visible: { opacity: 1, x: 0 },
         }}
+        transition={{ duration: 0.5, delay: 0.2 }}
       >
-        {["PROJECTS", "CONTACTS", "PENTESTING"].map((text, index) => {
-          const getHref = (index: number) => {
-            if (index === 0) return "#projects";
-            if (index === 1) return "#my-contacts";
-            return "/pentesting";
-          };
-
-          return (
-            <motion.a
-              key={text}
-              href={getHref(index)}
-              variants={{
-                hidden: { opacity: 0, y: 10 },
-                visible: { opacity: 1, y: 0 },
-              }}
-              whileHover={{ letterSpacing: "10px", color: "var(--accent)" }}
-              style={{ margin: "0 10px" }}
-            >
-              {text}
-            </motion.a>
-          );
-        })}
-      </motion.div>
-    );
-  };
-  
-
-function WelcomePage() {
-    return (
-        <motion.div 
-            className="welcome-block content"
+        <motion.div
+          className="welcome-content"
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: { opacity: 0, x: 20 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          transition={{ duration: 0.5, delay: 0.4 }}
+        >
+          <motion.h1
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5, delay: 0.6 }}
+          >
+            S.Artem
+          </motion.h1>
+          <motion.div
+            className="underline"
+            initial={{ width: 0 }}
+            animate={{ width: "100%" }}
+            transition={{ duration: 0.5, delay: 0.8 }}
+          />
+          <motion.div
+            style={{ width: "100%", gap: "0px" }}
             initial="hidden"
             animate="visible"
-            style={{ backgroundColor: "var(--bg-2)" }}
             variants={{
-                hidden: { opacity: 0, y: 20 },
-                visible: { opacity: 1, y: 0 }
+              hidden: { opacity: 0 },
+              visible: { opacity: 1 },
             }}
-            transition={{ duration: 0.5 }}
-        >
-            <Navigation />
-            <motion.div 
-                className="welcome-tiling"
-                initial="hidden"
-                animate="visible"
-                variants={{
-                    hidden: { opacity: 0, x: -20 },
-                    visible: { opacity: 1, x: 0 }
-                }}
-                transition={{ duration: 0.5, delay: 0.2 }}
+            transition={{ duration: 0.5, delay: 1 }}
+          >
+            <motion.p
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.3, delay: 1.2 }}
             >
-                <motion.div 
-                    className="welcome-content"
-                    initial="hidden"
-                    animate="visible"
-                    variants={{
-                        hidden: { opacity: 0, x: 20 },
-                        visible: { opacity: 1, x: 0 }
-                    }}
-                    transition={{ duration: 0.5, delay: 0.4 }}
-                >
-                    <motion.h1
-                        initial={{ opacity: 0, scale: 0.8 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        transition={{ duration: 0.5, delay: 0.6 }}
-                    >
-                        S.Artem
-                    </motion.h1>
-                    <motion.div 
-                        className="underline"
-                        initial={{ width: 0 }}
-                        animate={{ width: "100%" }}
-                        transition={{ duration: 0.5, delay: 0.8 }}
-                    />
-                    <motion.div 
-                        style={{ width: "100%", gap: "0px" }}
-                        initial="hidden"
-                        animate="visible"
-                        variants={{
-                            hidden: { opacity: 0 },
-                            visible: { opacity: 1 }
-                        }}
-                        transition={{ duration: 0.5, delay: 1 }}
-                    >
-                        <motion.p 
-                            initial={{ y: 20, opacity: 0 }}
-                            animate={{ y: 0, opacity: 1 }}
-                            transition={{ duration: 0.3, delay: 1.2 }}
-                        >
-                            Backend / ML Developer
-                        </motion.p>
-                        <motion.p 
-                            initial={{ y: 20, opacity: 0 }}
-                            animate={{ y: 0, opacity: 1 }}
-                            transition={{ duration: 0.3, delay: 1.4 }}
-                        >
-                            Freelancer
-                        </motion.p>
-                    </motion.div>
-                </motion.div>
-                <motion.img 
-                    src="me.png" 
-                    className="avatar-image"
-                    initial={{ y: 100, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    transition={{ duration: 0.5, delay: 1 }}
-                />
-            </motion.div>
+              Backend Developer
+            </motion.p>
+            <motion.p
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.3, delay: 1.4 }}
+            >
+              Freelancer
+            </motion.p>
+          </motion.div>
         </motion.div>
-    );
+        <motion.img
+          src="me.png"
+          className="avatar-image"
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 1 }}
+        />
+      </motion.div>
+    </motion.div>
+  );
 }
 
 export default WelcomePage;
